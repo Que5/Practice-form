47af6bb36bfefb95f21668070af661bb2d1b414c
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-10 10:22:43 +0200

    Form view added

--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Form
+from .forms import ContactForm
 
 # Create your views here.
+
+class FormDetail(DetailView):
+    model = Form
+    template_name = "form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = ContactForm()
+        return context
+
