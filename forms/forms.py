80d0956e752b9d51832e6557b310e56a827370d3
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-10 10:55:50 +0200

    Requirements txt file added

--- forms/forms.py
+++ forms/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Form
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
+# from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Form
-        widgets = {
-            'mobile_number': PhoneNumberPrefixWidget(),
-        }
         fields = "__all__"
+        # widgets = {
+        #     'mobile_number': PhoneNumberPrefixWidget(),
+        # }
--- forms/urls.py
+++ forms/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FormDetail
 
 urlpatterns = [
-    path("", FormDetail.as_view, name="form"),
+    path("<int:pk>/detail", FormDetail.as_view(), name="form"),
 ]
